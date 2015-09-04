cb558496dc9e33219303170d5a7ad6dd06746c35
Anonyo Hossain <anonyo.hossain@gmail.com>
2015-09-04 10:03:14 -0400

    Add discount and tip calculation to initialize of TotalCalculator

--- lib/total_calculator.rb
+++ lib/total_calculator.rb
@@ -1,21 +1,20 @@
 class TotalCalculator < Base
+  attr_reader :discount, :tip
+
+  def initialize(args)
+    super
+    @discount = AmountPercentage.new(@amount, @discount_percentage)
+    @tip = AmountPercentage.new(@amount, @tip_percentage)
+  end
 
   def calculate
-    amount_with_tax.calculate - discount_amount.calculate + tip_amount.calculate
+    amount_with_tax.calculate - discount.calculate + tip.calculate
   end
 
   private
 
   def amount_with_tax
     Tax.new(amount)
-  end
-
-  def discount_amount
-    AmountPercentage.new(amount, discount_percentage)
-  end
-
-  def tip_amount
-    AmountPercentage.new(amount, tip_percentage)
   end
 end
 
